00a64eda193bfdac30fdc32d4872194295cd2289
SyntaxSpin <86142168+SyntaxSpin@users.noreply.github.com>
2026-05-16 17:48:41 +0100

    updated droid sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "dev.rimehrab.tasuku"
-    compileSdk = 36
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "dev.rimehrab.tasuku"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
