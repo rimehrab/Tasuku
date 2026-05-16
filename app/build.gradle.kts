f8b971089e8f534d32871e43e3d903be4292838f
SyntaxSpin <86142168+SyntaxSpin@users.noreply.github.com>
2026-05-16 18:18:48 +0100

    updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.5.0-alpha19")
+    implementation("androidx.compose.material3:material3:1.5.0-alpha15")
     implementation("androidx.compose.material:material-icons-extended")
 
     val roomVersion = "2.6.1"
